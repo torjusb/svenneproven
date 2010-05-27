f53b73f79d01fabe34a6e65217650689988451be
nifty <nifty02@gmail.com>
2010-05-27 10:08:33 +0200

    changes

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@ get_header();
 ?>
 <div id="content" class="narrowcolumn">
 	<article>
-		<h1>Feilmelding 404</h1>
+		<h1>Feilmelding 404!</h1>
 		<div class="entry"><p>Gratulerer, du har ødelagt internett.</p></div>
 	</article>
 </div>
